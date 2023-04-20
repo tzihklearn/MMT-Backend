7eb8b823cdc28518c985467a0f135611fa824d25
DoudiNCer <doudi@outlook.lv>
2023-04-20 19:40:01 +0800

    Init SpringBoot Project

---
+++ src/test/java/com/sipc/mmtbackend/MmtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipc.mmtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
