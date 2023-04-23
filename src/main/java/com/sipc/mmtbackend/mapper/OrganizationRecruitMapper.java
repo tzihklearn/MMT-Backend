5807cd24d4df08e659da6340ce8f8aa8e76592c2
tzihklearn <1922941549@qq.com>
2023-04-23 21:42:17 +0800

    feat(*):社团宣传信息实体类配置

---
+++ src/main/java/com/sipc/mmtbackend/mapper/OrganizationRecruitMapper.java
@@ -0,0 +1,18 @@
+package com.sipc.mmtbackend.mapper;
+
+import com.sipc.mmtbackend.pojo.domain.OrganizationRecruit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author tzih
+ * @since 2023-04-23
+ */
+@Mapper
+public interface OrganizationRecruitMapper extends BaseMapper<OrganizationRecruit> {
+
+}
---
+++ src/main/java/com/sipc/mmtbackend/pojo/domain/Department.java
@@ -0,0 +1,40 @@
+package com.sipc.mmtbackend.pojo.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author tzih
+ * @since 2023-04-23
+ */
+@Getter
+@Setter
+@TableName("department")
+public class Department implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @TableField("name")
+    private String name;
+
+    @TableField("brief_description")
+    private String briefDescription;
+
+    @TableField("description")
+    private String description;
+
+    @TableField("standard")
+    private String standard;
+}
