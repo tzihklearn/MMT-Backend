a9009765baf9a2c12a9f7bf0b0d7142d922c1370
DoudiNCer <doudi@outlook.lv>
2023-05-29 20:44:20 +0800

    Feat：完成 B 端面试总看板服务获取组织或部门当前总报名人数与第一志愿人数接口

--- src/main/java/com/sipc/mmtbackend/controller/InterviewBoardController.java
+++ src/main/java/com/sipc/mmtbackend/controller/InterviewBoardController.java
@@ -3,9 +3,11 @@ package com.sipc.mmtbackend.controller;
 import com.sipc.mmtbackend.pojo.dto.CommonResult;
 import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResult.GetDepartmentsResult;
 import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResult.GetNumberGroupByDepartment;
+import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResult.GetSignUpNum;
 import com.sipc.mmtbackend.service.InterviewBoardService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -34,5 +36,16 @@ public class InterviewBoardController {
     @GetMapping("/numGroupByDepartment")
     public CommonResult<GetNumberGroupByDepartment> getNumberGroupByDepartment() {
         return interviewBoardService.getNumberGroupByDepartment();
+    }
+
+    /**
+     * 获取组织或指定部门的报名人数与第一志愿人数
+     *
+     * @param departmentId 组织ID
+     * @return 总人数与第一志愿人数
+     */
+    @GetMapping("/totalNum")
+    public CommonResult<GetSignUpNum> getSignupNum(@RequestParam(value = "departmentId", required = false) Integer departmentId) {
+        return interviewBoardService.getSignupNum(departmentId);
     }
 }
