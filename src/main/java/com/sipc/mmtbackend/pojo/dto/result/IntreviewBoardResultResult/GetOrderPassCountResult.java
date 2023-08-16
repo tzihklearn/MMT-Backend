961e204ea5bbcc226b1d60aefe08a91590972b80
DoudiNCer <doudi@outlook.lv>
2023-08-16 19:45:17 +0800

    Fix: 面试总看板/面试后/获取最终各部门通过人数（组织饼图）的Data没写

--- src/main/java/com/sipc/mmtbackend/pojo/dto/result/IntreviewBoardResultResult/GetOrderPassCountResult.java
+++ src/main/java/com/sipc/mmtbackend/pojo/dto/result/IntreviewBoardResultResult/GetOrderPassCountResult.java
@@ -1,7 +1,13 @@
 package com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResultResult;
 
+import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResultResult.po.OrderPassCountPo;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class GetOrderPassCountResult {
+    // 志愿数量
+    private Integer count;
+    private List<OrderPassCountPo> orders;
 }
---
+++ src/main/java/com/sipc/mmtbackend/pojo/dto/result/IntreviewBoardResultResult/po/OrderPassCountPo.java
@@ -0,0 +1,10 @@
+package com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResultResult.po;
+
+public class OrderPassCountPo {
+    // 志愿ID
+    private Integer id;
+    // 志愿名称
+    private String name;
+    // 志愿通过人数
+    private Integer count;
+}
