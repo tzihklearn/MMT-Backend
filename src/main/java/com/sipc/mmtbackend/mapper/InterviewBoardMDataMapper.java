95a6da5469732573d52112cd8cd969d2ada80f24
DoudiNCer <doudi@outlook.lv>
2023-08-17 18:36:50 +0800

    Feat: 面试总看板 面试中 获取面试进度 接口

--- src/main/java/com/sipc/mmtbackend/mapper/InterviewBoardMDataMapper.java
+++ src/main/java/com/sipc/mmtbackend/mapper/InterviewBoardMDataMapper.java
@@ -1,6 +1,7 @@
 package com.sipc.mmtbackend.mapper;
 
 import com.sipc.mmtbackend.pojo.domain.po.InterviewBoardMPo.CheckinInfoLPo;
+import com.sipc.mmtbackend.pojo.domain.po.InterviewBoardMPo.InterviewProgressPo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -18,12 +19,26 @@ public interface InterviewBoardMDataMapper {
      *
      * @param round 面试轮次
      * @param admissionId 纳新ID
-     * @param DepartmentId 部门ID
+     * @param departmentId 部门ID
      * @return 签到信息（学号、姓名、面试地点、签到时间）
      */
     List<CheckinInfoLPo> selectCheckinInfo(
             @Param("round") Integer round,
             @Param("admissionId") Integer admissionId,
-            @Param("departmentId") Integer DepartmentId
+            @Param("departmentId") Integer departmentId
+    );
+
+    /**
+     * 获取各个地点面试进度
+     *
+     * @param round 面试轮次
+     * @param admissionId 纳新ID
+     * @param departmentId 部门ID'
+     * @return 各地点面试进度
+     */
+    List<InterviewProgressPo> selectInterviewProgress(
+            @Param("round") Integer round,
+            @Param("admissionId") Integer admissionId,
+            @Param("departmentId") Integer departmentId
     );
 }
--- src/main/java/com/sipc/mmtbackend/service/impl/InterviewBoardMiddleServiceImpl.java
+++ src/main/java/com/sipc/mmtbackend/service/impl/InterviewBoardMiddleServiceImpl.java
@@ -6,11 +6,14 @@ import com.sipc.mmtbackend.mapper.InterviewCheckMapper;
 import com.sipc.mmtbackend.pojo.domain.Admission;
 import com.sipc.mmtbackend.pojo.domain.Department;
 import com.sipc.mmtbackend.pojo.domain.po.InterviewBoardMPo.CheckinInfoLPo;
+import com.sipc.mmtbackend.pojo.domain.po.InterviewBoardMPo.InterviewProgressPo;
 import com.sipc.mmtbackend.pojo.dto.CommonResult;
 import com.sipc.mmtbackend.pojo.dto.enums.InterviewRoundEnum;
 import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardMiddleResult.GetCheckinListResult;
+import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardMiddleResult.GetInterviewProgressCircleResult;
 import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardMiddleResult.GetInterviewRoundsResult;
 import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardMiddleResult.po.CheckinInfoPo;
+import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardMiddleResult.po.InterviewRoomProgressPo;
 import com.sipc.mmtbackend.pojo.dto.result.po.KVPo;
 import com.sipc.mmtbackend.service.InterviewBoardMiddleService;
 import com.sipc.mmtbackend.utils.CheckroleBUtil.pojo.BTokenSwapPo;
@@ -113,6 +116,55 @@ public class InterviewBoardMiddleServiceImpl implements InterviewBoardMiddleServ
         }
         result.setCount(checkinInfos.size());
         result.setCheckins(results);
+        return CommonResult.success(result);
+    }
+
+    /**
+     * 获取面试进度
+     *
+     * @param round        面试轮次，默认为1
+     * @param departmentId 部门 ID，默认0（全部部门）
+     * @return 各个面试地点的面试进度
+     */
+    @Override
+    public CommonResult<GetInterviewProgressCircleResult> getInterviewProgressCircle(int round, int departmentId) {
+        BTokenSwapPo context = ThreadLocalContextUtil.getContext();
+        Admission admission = interviewCheckMapper.selectOrganizationActivateAdmission(context.getOrganizationId());
+        if (admission == null) {
+            log.warn("用户 " + context + " 尝试在无活动的纳新时查询已开始面试轮次");
+            return CommonResult.fail("查询失败：未开始纳新或纳新已结束");
+        }
+        Integer maxRound = interviewCheckMapper.selectOrganizationActivateInterviewRound(admission.getId());
+        if (maxRound == null){
+            log.warn("用户 " + context + " 在纳新 " + admission + " 中未查询到任何面试");
+            return CommonResult.fail("当前纳新未开启面试");
+        }
+        if (round > maxRound){
+            log.warn("用户 " + context + " 在纳新 " + admission + " 请求不存在的面试轮次 " + round + "/" + maxRound);
+            return CommonResult.fail("指定的面试轮次还未开始");
+        }
+        if (departmentId != 0){
+            Department department = departmentMapper.selectById(departmentId);
+            if (department == null){
+                log.warn("用户 " + context + " 在纳新 " + admission + " 请求不存在的部门" + departmentId + "的签到列表");
+                return CommonResult.fail("部门不存在或不属于当前组织");
+            } else if (!Objects.equals(department.getOrganizationId(), context.getOrganizationId())){
+                log.warn("用户 " + context + " 在纳新 " + admission + " 请求不属于其组织的的部门" + department + "的签到列表");
+                return CommonResult.fail("部门不存在或不属于当前组织");
+            }
+        }
+        List<InterviewProgressPo> progress = interviewBoardMDataMapper.selectInterviewProgress(round, admission.getId(), departmentId);
+        GetInterviewProgressCircleResult result = new GetInterviewProgressCircleResult();
+        List<InterviewRoomProgressPo> results = new ArrayList<>();
+        for (InterviewProgressPo ipp : progress) {
+            InterviewRoomProgressPo irpp = new InterviewRoomProgressPo();
+            irpp.setName(ipp.getName());
+            irpp.setTotal(ipp.getTotal());
+            irpp.setFinished(ipp.getFinished());
+            results.add(irpp);
+        }
+        result.setRooms(results);
+        result.setCount(results.size());
         return CommonResult.success(result);
     }
 }
