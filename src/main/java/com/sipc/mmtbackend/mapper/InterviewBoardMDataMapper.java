b6766f5fbcf27c144677c0217f8840fe17cadecb
DoudiNCer <doudi@outlook.lv>
2023-08-17 15:38:19 +0800

    Chore： 面试总看板自定义 Mapper 区分面试前（B）、中（M）、后（R）

---
+++ src/main/java/com/sipc/mmtbackend/mapper/InterviewBoardMDataMapper.java
@@ -0,0 +1,13 @@
+package com.sipc.mmtbackend.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 面试看板（面试中）数据查询 Mapper
+ *
+ * @author DoudiNCer
+ */
+@Mapper
+public interface InterviewBoardMDataMapper {
+
+}
