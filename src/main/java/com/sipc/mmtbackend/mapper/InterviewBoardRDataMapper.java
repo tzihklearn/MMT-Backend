47405b465e73c6d8e7cf4a1b1db637ea3a230392
DoudiNCer <doudi@outlook.lv>
2023-08-19 20:24:44 +0800

    Feat: 面试总看板 面试后 获取最终各志愿通过人数（部门饼图） 接口

--- src/main/java/com/sipc/mmtbackend/mapper/InterviewBoardRDataMapper.java
+++ src/main/java/com/sipc/mmtbackend/mapper/InterviewBoardRDataMapper.java
@@ -3,6 +3,7 @@ package com.sipc.mmtbackend.mapper;
 import com.sipc.mmtbackend.pojo.domain.po.InterviewBoardRPo.DepartmentPassedCountPo;
 import com.sipc.mmtbackend.pojo.domain.po.InterviewBoardRPo.InterviewResultData;
 import com.sipc.mmtbackend.pojo.domain.po.InterviewBoardRPo.LineChartLineDataDaoPo;
+import com.sipc.mmtbackend.pojo.domain.po.InterviewBoardRPo.OrderPassedCountPo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -31,7 +32,7 @@ public interface InterviewBoardRDataMapper {
     );
 
     /**
-     * 查询各个部门通过i人数
+     * 查询各个部门通过人数
      *
      * @param round 面试轮次
      * @param admissionId 纳新ID
@@ -50,4 +51,18 @@ public interface InterviewBoardRDataMapper {
      */
     List<LineChartLineDataDaoPo> selectPassedCountLineChartGroupByRoundByAdmissionId(
             @Param("admissionId") Integer admissionId);
+
+    /**
+     * 查询最终各个志愿通过人数
+     *
+     * @param admissionId 纳新ID
+     * @param departmentId 部门ID
+     * @param round 年事轮次
+     * @return 最终各个志愿通过人数
+     */
+    List<OrderPassedCountPo> selectPassedCountPerOrder(
+            @Param("round") Integer round,
+            @Param("admissionId") Integer admissionId,
+            @Param("departmentId") Integer departmentId
+    );
 }
