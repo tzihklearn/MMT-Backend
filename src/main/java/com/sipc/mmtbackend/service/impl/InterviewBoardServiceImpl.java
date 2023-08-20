1c55a8a57bcc447e45d2070e9fbea21205ea21bd
DoudiNCer <doudi@outlook.lv>
2023-08-20 20:00:58 +0800

    Feat：面试总看板 获取当前纳新状态 接口

--- src/main/java/com/sipc/mmtbackend/service/impl/InterviewBoardServiceImpl.java
+++ src/main/java/com/sipc/mmtbackend/service/impl/InterviewBoardServiceImpl.java
@@ -2,9 +2,14 @@ package com.sipc.mmtbackend.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sipc.mmtbackend.mapper.DepartmentMapper;
+import com.sipc.mmtbackend.mapper.InterviewBoardDataMapper;
+import com.sipc.mmtbackend.mapper.InterviewCheckMapper;
+import com.sipc.mmtbackend.pojo.domain.Admission;
 import com.sipc.mmtbackend.pojo.domain.Department;
+import com.sipc.mmtbackend.pojo.domain.InterviewStatus;
 import com.sipc.mmtbackend.pojo.dto.CommonResult;
 import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResult.GetDepartmentsResult;
+import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResult.GetIntreviewStatusResult;
 import com.sipc.mmtbackend.pojo.dto.result.IntreviewBoardResult.po.GetDepartmentPo;
 import com.sipc.mmtbackend.service.InterviewBoardService;
 import com.sipc.mmtbackend.utils.CheckroleBUtil.pojo.BTokenSwapPo;
@@ -14,6 +19,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.jnlp.IntegrationService;
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -22,6 +29,8 @@ import java.util.List;
 @Slf4j
 public class InterviewBoardServiceImpl implements InterviewBoardService {
     private final DepartmentMapper departmentMapper;
+    private final InterviewCheckMapper interviewCheckMapper;
+    private InterviewBoardDataMapper interviewBoardDataMapper;
 
     /**
      * 获取当前登录组织的部门列表
@@ -44,6 +53,62 @@ public class InterviewBoardServiceImpl implements InterviewBoardService {
         }
         result.setNum(departments.size());
         result.setDepartments(departments);
+        return CommonResult.success(result);
+    }
+
+    /**
+     * 获取当前纳新状态
+     *
+     * @return 当前纳新状态 0未开始，1正在进行，2已结束
+     */
+    @Override
+    public CommonResult<GetIntreviewStatusResult> getInterviewStatus() {
+        BTokenSwapPo context = ThreadLocalContextUtil.getContext();
+        Admission admission = interviewCheckMapper.selectOrganizationActivateAdmission(context.getOrganizationId());
+        // 无活动纳新
+        if (admission == null) {
+            log.warn("用户 " + context + " 尝试在无活动的纳新时查询纳新状态");
+            return CommonResult.fail("查询失败：未开始纳新或纳新已结束");
+        }
+        log.info("用户 " + context + " 开始判断纳新 " + admission + " 的状态\n");
+        GetIntreviewStatusResult result = new GetIntreviewStatusResult();
+        Integer checkinCount = interviewBoardDataMapper.selectCountOfCheckinInterview(admission.getId());
+        // 无人签到，面试前
+        if (checkinCount == 0){
+            log.info("第一轮已签到人数为0，面试未开始\n");
+            result.setStatus(0);
+            return CommonResult.success(result);
+        }
+        log.info("\t第一轮已签到人数：" + checkinCount + "\n");
+        Integer maxRound = interviewCheckMapper.selectOrganizationActivateInterviewRound(admission.getId());
+        // 最新一轮不是计划的最后一轮面试
+        if (maxRound < admission.getRounds()){
+            log.info("最后一轮未开始，面试未结束\n");
+            result.setStatus(1);
+            return CommonResult.success(result);
+        }
+        log.info("\t最后一轮（第 " + maxRound + " 轮）已开始\n");
+        List<InterviewStatus> notFinishedInterview = interviewBoardDataMapper.selectNotFinishedInterview(
+                admission.getId(), maxRound);
+        int notFinishedCount = notFinishedInterview.size();
+        // 最后一轮面试存在状态不是“待定”的面试
+        if (notFinishedCount == 0){
+            log.info("最后一轮面试已全部结束\n");
+            result.setStatus(2);
+            return CommonResult.success(result);
+        }
+        log.info("\t最后一轮面试未结束\n");
+        InterviewStatus last = notFinishedInterview.get(0);
+        // 最后一轮未手动结束面试的面试结束时间已过
+        if (last.getEndTime().isBefore(LocalDateTime.now())){
+            log.warn("最后一轮面试最后一场结束时间已过，现在是" + LocalDateTime.now() + "\n");
+            for (InterviewStatus i : notFinishedInterview) {
+                log.warn("\t面试 " + i + " 已超过其面试结束时间 " + i.getEndTime() + " 但未结束\n");
+            }
+            result.setStatus(2);
+            return CommonResult.success(result);
+        }
+        result.setStatus(1);
         return CommonResult.success(result);
     }
 
