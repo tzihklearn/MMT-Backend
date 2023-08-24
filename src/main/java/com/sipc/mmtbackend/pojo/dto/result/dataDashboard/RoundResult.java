1ffee67e1e75632b2ef80967e4027bb461a4ff83
tzihklearn <1922941549@qq.com>
2023-08-25 00:27:22 +0800

    feat(*)：新增获取面试评价面试轮次的接口

---
+++ src/main/java/com/sipc/mmtbackend/pojo/dto/result/dataDashboard/RoundResult.java
@@ -0,0 +1,15 @@
+package com.sipc.mmtbackend.pojo.dto.result.dataDashboard;
+
+import lombok.Data;
+
+/**
+ * @author tzih
+ * @version v1.0
+ * @since 2023.08.25
+ */
+@Data
+public class RoundResult {
+
+    private Integer round;
+
+}
